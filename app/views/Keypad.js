bc16a2da0689dd190814260e67df9cce66bb9f85
Naren Iyer <naren.iyer@evolutioncrm.co.uk>
2019-05-14 15:39:48 +0100

    Attempt to get id of button press. Doesn't work

--- app/views/Keypad.js
+++ app/views/Keypad.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
-import { Container, View, Text, Button} from 'native-base';
+import { Container, View, Text, Button } from 'native-base';
+// import { Button } from 'react-native-elements'
 import { StyleSheet, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export class Keypad extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             numberField: '07780687234'
         }
-        this.registerNumber = this.registerNumber.bind(this);
+        this.call = this.call.bind(this);
+        // this.renderKeypad = this.renderKeypad.bind(this);
     }
 
-    registerNumber = (event) => {
-        console.log("Number: ", event.target.id);
+    componentDidMount = () => {
+        // this.renderKeypad();
+    }
+
+    registerNumber = () => {
+        console.log();
+    }
+
+    call = () => { }
+
+    renderTop = () => {
+        const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
+        const topnums = [];
+        for (index = 1; index < numbers.length + 1; index++) {
+            topnums.push(
+                // <Button style={style.keypad} key={index} id={index} onPress={this.registerNumber.bind(this, index)}>
+                //     <Text style={style.keyText}>{index}</Text>
+                // </Button>
+            )
+        }
+        return topnums;
     }
 
     render() {
+        let topnums = this.renderTop();
         return (
             <Container style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <TextInput style={{marginBottom: 20, fontSize: 40}}>{this.state.numberField}</TextInput>
+                <TextInput style={{ marginBottom: 20, fontSize: 40 }}>{this.state.numberField}</TextInput>
                 <View style={{ alignItem: 'center', flexDirection: 'row', marginTop: 10 }} pointerEvents="auto">
-                    <Button style={style.keypad} onPress={(event) => this.registerNumber(event)}>
-                        <Text style={style.keyText} value="1">1</Text>
+                    <Button title={buttonObj.one} style={style.keypad} onPress={(event) => this.registerNumber(event)}>
+                        <Text style={style.keyText}>1</Text>
                     </Button>
-                    <Button style={style.keypad} onPress={() => this.registerNumber()}>
+                    <Button style={style.keypad} onPress={(title) => this.registerNumber(title)}>
                         <Text style={style.keyText}>2</Text>
                     </Button>
                     <Button style={style.keypad} onPress={() => this.registerNumber()}>
@@ -67,7 +89,7 @@ export class Keypad extends Component {
                 </View>
                 <View style={{ alignItem: 'center', flexDirection: 'row', marginTop: 10 }}>
                     <View style={style.keypad}>
-                        <Icon name="phone" style={{color: 'green', fontSize: 20}} />
+                        <Icon name="phone" style={{ color: 'green', fontSize: 20 }} />
                     </View>
                 </View>
             </Container>
@@ -79,11 +101,11 @@ const style = StyleSheet.create({
     keypad: {
         marginLeft: 5,
         marginRight: 10,
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        height: 70, 
-        width: 70, 
-        borderRadius: 140, 
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 70,
+        width: 70,
+        borderRadius: 140,
         backgroundColor: '#E1E2EF',
         fontSize: 10
     },
@@ -95,7 +117,17 @@ const style = StyleSheet.create({
     }
 });
 
-const topkeys = ["1", "2", "3"];
-const midkeys = ["4", "5", "6"];
-const btmkeys = ["7", "8", "9"];
-const specialKeys = ["*", "0", "#"];
+const buttonObj = {
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+    hash: "#",
+    star: "*"
+}
